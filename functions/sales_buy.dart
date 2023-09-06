11edf90494b602109c1e2d04f522ec631b50d912
Israr Ahmed <sakijaved3@gmail.com>
2023-09-07 01:48:33 +0500

    first app

--- functions/sales_buy.dart
+++ functions/sales_buy.dart
@@ -11,20 +11,23 @@ class _SalesBuyState extends State<SalesBuy> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: 
-      ListView(
-        scrollDirection:Axis.horizontal,
-        children: [
-          Card(color:Color(0xffF9B023,
-        
-),
-child: Row(
-  children: [
-   Icon(Icons.image_rounded),
-  ],
-),)
-        ],
-      ),),
+      body: SafeArea(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Card(
+              color: Color(
+                0xffF9B023,
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.image_rounded),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
